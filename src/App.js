eb09f505a87d9b7bb0610884e7ea19162a8b0644
dosxanzada <dosxanzada@gmail.com>
2020-10-20 01:40:37 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Beka</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,6 +22,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import { getDefaultNormalizer } from "@testing-library/react";
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <h1>this is header</h1>
+    </div>
+  );
+}
+
+export default Header;
+
+// let app = document.querySelector("#app");
